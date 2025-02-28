46cfb6e4369f7139a73c7cf04c6e35276d763824
Raffaele Rizzardi <rizzardi.raffaele@iisgalvanimi.edu.it>
2025-02-28 14:44:41 +0000

    ripristinato programma a versione funzionante

---
+++ services/spotify_oauth.py
@@ -0,0 +1,17 @@
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+SPOTIFY_CLIENT_ID = "b1ed2d5909a24d66912adc6f42531fee"
+SPOTIFY_CLIENT_SECRET = "db711b1f9ba646b1a5d1c4cbe0cf897b"
+SPOTIFY_REDIRECT_URI = "https://5000-raffomagno-progettonesp-72o7xu40jhr.ws-eu118.gitpod.io/callback"
+
+sp_oauth = SpotifyOAuth (
+    client_id=SPOTIFY_CLIENT_ID,
+    client_secret=SPOTIFY_CLIENT_SECRET,
+    redirect_uri=SPOTIFY_REDIRECT_URI,
+    scope="user-read-private",
+    show_dialog=True
+)
+
+def get_spotify_object(token_info):
+    return spotipy.Spotify(auth=token_info['access_token'])
