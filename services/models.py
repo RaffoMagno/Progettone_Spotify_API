fd39b9d6366225ca7951597f6215750b235ee896
messex04 messex04 <messina.daniele.2004@gmail.com>
2025-03-24 11:43:37 +0000

    parte messina

--- services/models.py
+++ services/models.py
@@ -33,3 +33,52 @@ class DatabaseWrapper:
             result = cursor.fetchall()
         conn.close()
         return result #restituisce il risultato delle select
+
+        def create_table_Utente(self):
+            self.execute_query('''
+            CREATE TABLE IF NOT EXISTS Utente (
+                id_u INT AUTO_INCREMENT PRIMARY KEY,
+                nickname VARCHAR(100) NOT NULL,
+                password VARCHAR(100) NOT NULL,
+                
+            )
+        ''')
+        
+        def create_table_playlist(self):
+            self.execute_query('''
+            CREATE TABLE IF NOT EXISTS Playlist (
+                id_p VARCHAR(255) PRIMARY KEY,
+                id_u INT NOT NULL,
+                
+            )
+        ''')
+    def get_Utente(self):
+        return self.fetch_query('SELECT * FROM Utente')
+    
+    def get_Playlist(self):
+        return self.fetch_query('SELECT * FROM Playlist')
+
+
+    #manipolazione tabelle con execute_query!!!!
+    def aggiungi_Utente(self, nickname, password):
+        self.execute_query('INSERT INTO Utente (nickname, password) VALUES (%s)', (nickname, password,))
+        
+    def aggiungi_Playlist(self, nickname, password):
+        self.execute_query('INSERT INTO Utente (nickname, password) VALUES (%s)', (nickname, password,))
+        
+        
+    #%s fa da placeholder: "la variabile che passo come parametro prende il valore di %s"
+
+    def rimuovi_Playlist(self, indice):
+        self.execute_query('DELETE FROM Playlist WHERE id_p = %s', (indice,))
+
+    def rimuovi_Utente(self, indice):
+        self.execute_query('DELETE FROM Utente WHERE id_u = %s', (indice,))
+
+    def svuota_Utente(self):
+        self.execute_query('DELETE FROM Utente')
+
+    def svuota_Playlist(self):
+        self.execute_query('DELETE FROM Playlist')
+
+
