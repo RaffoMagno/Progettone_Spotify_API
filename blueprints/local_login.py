706dc173803e815308b5c335ba2def3477822bd3
Raffaele Rizzardi <rizzardi.raffaele@iisgalvanimi.edu.it>
2025-04-11 15:39:04 +0000

    aggiunto controllo password

--- blueprints/local_login.py
+++ blueprints/local_login.py
@@ -1,9 +1,28 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
-from services.models import db, User  # Importa correttamente db
+from services.models import db, User
 from werkzeug.security import check_password_hash, generate_password_hash
 from flask_login import login_user
+import re  # Import necessario per controllaPassword
 
 local_login_bp = Blueprint('local_login', __name__)
+
+def controllaPassword(password):
+    if len(password) < 8:
+        flash("La password deve contenere almeno 8 caratteri.", "error")
+        return False
+    if not re.search(r"[A-Z]", password):
+        flash("La password deve contenere almeno una lettera maiuscola.", "error")
+        return False
+    if not re.search(r"[a-z]", password):
+        flash("La password deve contenere almeno una lettera minuscola.", "error")
+        return False
+    if not re.search(r"[0-9]", password):
+        flash("La password deve contenere almeno un numero.", "error")
+        return False
+    if not re.search(r"[!@#$%^&*(),.?\":{}|<>]", password):
+        flash("La password deve contenere almeno un carattere speciale.", "error")
+        return False
+    return True
 
 @local_login_bp.route('/', methods=['GET', 'POST'])
 def login_page():
@@ -11,14 +30,13 @@ def login_page():
         username = request.form['nickname']
         password = request.form['password']
 
-        utenti = db.get_Utente()  # Ora puoi usare db perché è stato importato
-        user_data = next((u for u in utenti if u[0] == username), None)  # Cerca l'utente per nickname
+        utenti = db.get_Utente()
+        user_data = next((u for u in utenti if u[0] == username), None)
         
         if user_data:
-            stored_nickname, stored_password = user_data  # stored_password è la password memorizzata
-
-            if stored_password == password:  # Confronta direttamente le password
-                user = User(nickname=stored_nickname)  # Assumendo che User sia correttamente definito
+            stored_nickname, stored_password = user_data
+            if stored_password == password:
+                user = User(nickname=stored_nickname)
                 login_user(user)
                 flash("Login effettuato!", "success")
                 return redirect(url_for('home.homepage'))
@@ -28,21 +46,21 @@ def login_page():
     
     return render_template('login.html')
 
-
 @local_login_bp.route('/register', methods=['GET', 'POST'])
 def register_page():
     if request.method == 'POST':
         nickname = request.form['nickname']
         password = request.form['password']
 
-        # Controlliamo se l'utente esiste già
-        utenti = db.get_Utente()  # Usa db per accedere agli utenti
+        if not controllaPassword(password):
+            return redirect(url_for('local_login.register_page'))
+
+        utenti = db.get_Utente()
         if any(u[0] == nickname for u in utenti):
             flash('Nome utente già esistente!', 'error')
             return redirect(url_for('local_login.register_page'))
 
-        # Salviamo l'utente con password in chiaro
-        db.aggiungi_Utente(nickname, password)  # Salva la password in chiaro
+        db.aggiungi_Utente(nickname, password)
         flash('Registrazione completata con successo!', 'success')
         return redirect(url_for('local_login.login_page'))
 
